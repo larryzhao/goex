3261b185fbc7d69952e20d07459487b44b8acb28
Larry <thehiddendepth@gmail.com>
2019-11-22 18:57:16 +0800

    add MinInt

--- mathx/mathx.go
+++ mathx/mathx.go
@@ -8,6 +8,13 @@ func MaxInt(a int, b int) int {
 	return b
 }
 
+func MinInt(a int, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
 func MaxInt64(a int64, b int64) int64 {
 	if a >= b {
 		return a
