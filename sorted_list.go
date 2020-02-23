d8003a6348a57f97fffb896c43e3a842dec7de4b
Larry <thehiddendepth@gmail.com>
2020-02-23 16:46:02 +0800

    add a simple sorted_list

---
+++ sorted_list.go
@@ -0,0 +1,220 @@
+package goex
+
+import (
+	"math"
+	"strconv"
+	"sync"
+)
+
+
+var maxScore int64 = math.MaxInt64
+var minScore int64 = math.MinInt64
+
+type sortedListItem struct {
+	Val         interface{}
+	Score 		int64
+	Next        *sortedListItem
+	Prev		*sortedListItem
+}
+
+type SortedList struct {
+	head *sortedListItem
+	tail *sortedListItem
+	mu   *sync.Mutex
+	len  int
+}
+
+// NewSortedList create a sorted list
+func NewSortedList() *SortedList {
+	return &SortedList{
+		head: nil,
+		tail: nil,
+		mu:   &sync.Mutex{},
+		len:  0,
+	}
+}
+
+// Len length of the current list
+func (list *SortedList) Len() int {
+	return list.len
+}
+
+func (list *SortedList) Add(score int64, val interface{}) {
+	defer list.mu.Unlock()
+	list.mu.Lock()
+
+	newItem := &sortedListItem{
+		Val:   val,
+		Score: score,
+		Next:  nil,
+		Prev: 	nil,
+	}
+
+	// when list is empty, the item is the first
+	if list.Empty() {
+		list.head = newItem
+		list.tail = newItem
+		list.len++
+		return
+	}
+
+	// when it should be the new head
+	if score <= list.head.Score {
+		newItem.Next = list.head
+		list.head.Prev = newItem
+
+		list.head = newItem
+		list.len++
+		return
+	}
+
+	prev := list.head
+	current := list.head.Next
+
+	// when there's only the head, and the item should go after it
+	if current == nil {
+		prev.Next = newItem
+		newItem.Prev = prev
+		list.tail = newItem
+		list.len++
+		return
+	}
+
+	for {
+		// found the place to be
+		if score <= current.Score {
+			prev.Next = newItem
+			newItem.Prev = prev
+
+			newItem.Next = current
+			current.Prev = newItem
+
+			list.len++
+			return
+		}
+
+		// when we meets the tail
+		if current.Next == nil {
+			current.Next = newItem
+			newItem.Prev = current
+			list.tail = newItem
+			list.len++
+			return
+		}
+
+		// move next
+		prev = current
+		current = prev.Next
+	}
+}
+
+func (list *SortedList) Empty() bool {
+	return list.head == nil
+}
+
+func (list *SortedList) PopScoreLowerThan(limit string, count uint32) []interface{} {
+	var current *sortedListItem
+	var idx uint32
+
+	if count == 0 || limit == "-inf" || list.Empty() {
+		return []interface{}{}
+	}
+
+	up := upperComparer(limit)
+
+	var results []interface{}
+
+	list.mu.Lock()
+	defer func() {
+		if list.head != current {
+			list.head = current
+			list.len = list.len - int(idx)
+		}
+		list.mu.Unlock()
+	}()
+
+	current = list.head
+	for idx < count {
+		if !up(current.Score) {
+			return results
+		}
+
+		results = append(results, current.Val)
+		current = current.Next
+		idx++
+	}
+
+	return results
+}
+
+//func (list *SortedList) PopByScore(lower string, upper string, count uint32) []interface{} {
+//	lc := lowComparer(lower)
+//	uc := upperComparer(upper)
+//
+//	results := make([]interface{}, count)
+//	defer list.mu.Unlock()
+//	list.mu.Lock()
+//
+//	idx := 0
+//	for idx < count {
+//
+//	}
+//}
+
+func lowComparer(limit string) func(score int64) bool {
+	if limit == "-inf" {
+		return func(score int64) bool {
+			return score >= minScore
+		}
+	}
+
+	if limit == "+inf" {
+		return func(score int64) bool {
+			return score >= maxScore
+		}
+	}
+
+	rr := []rune(limit)
+
+	if rr[0] == '(' {
+		// TODO: handle error
+		limitScore, _ := strconv.ParseInt(string(rr[1:]), 10, 64)
+		return func(score int64) bool {
+			return score > limitScore
+		}
+	}
+
+	limitScore, _ := strconv.ParseInt(limit, 10, 64)
+	return func(score int64) bool {
+		return score >= limitScore
+	}
+}
+
+func upperComparer(limit string) func(score int64) bool {
+	if limit == "-inf" {
+		return func(score int64) bool {
+			return score <= minScore
+		}
+	}
+
+	if limit == "+inf" {
+		return func(score int64) bool {
+			return score <= maxScore
+		}
+	}
+
+	rr := []rune(limit)
+
+	if rr[0] == '(' {
+		// TODO: handle error
+		limitScore, _ := strconv.ParseInt(string(rr[1:]), 10, 64)
+		return func(score int64) bool {
+			return score < limitScore
+		}
+	}
+
+	limitScore, _ := strconv.ParseInt(limit, 10, 64)
+	return func(score int64) bool {
+		return score <= limitScore
+	}
+}
---
+++ sorted_list_test.go
@@ -0,0 +1,122 @@
+package goex
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestNewSortedList(t *testing.T) {
+	list := NewSortedList()
+	assert.NotNil(t, list)
+	assert.Equal(t, 0, list.len)
+	assert.Nil(t, list.head)
+}
+
+func TestSortedListAdd(t *testing.T) {
+	var list *SortedList
+	// test first item add
+	list = NewSortedList()
+	list.Add(1000, "a")
+
+	assert.Equal(t, 1, list.len)
+	assert.NotNil(t, list.head)
+	assert.Exactly(t, extractListToSlice(list), []string{"a"})
+
+	// test if the item should be the new head
+	list.Add(900, "b")
+	assert.Equal(t, 2, list.Len())
+	assert.Exactly(t, extractListToSlice(list), []string{"b", "a"})
+
+	// test if the item should be at the tail
+	list.Add(1100, "c")
+	assert.Equal(t, 3, list.Len())
+	assert.Exactly(t, extractListToSlice(list), []string{"b", "a", "c"})
+	assert.Equal(t, "c", list.tail.Val.(string))
+
+	// test if the item should be in the middle
+	list.Add(1020, "d")
+	assert.Equal(t, 4, list.Len())
+	assert.Equal(t, "d", list.head.Next.Next.Val.(string))
+	assert.Exactly(t, extractListToSlice(list), []string{"b", "a", "d", "c"})
+}
+
+func TestPopScoreLowerThan(t *testing.T) {
+	list := NewSortedList()
+	list.Add(1000, "a")
+	list.Add(2000, "b")
+	list.Add(3000, "c")
+	list.Add(4000, "d")
+	list.Add(5000, "e")
+	list.Add(6000, "f")
+	list.Add(7000, "g")
+	list.Add(8000, "h")
+	list.Add(9000, "i")
+
+	assert.Equal(t, 9, list.Len())
+	assert.Exactly(t, extractListToSlice(list), []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"})
+
+	items := list.PopScoreLowerThan("3000", 2)
+	assert.Equal(t, 2, len(items))
+	assert.Equal(t, "a", items[0].(string))
+	assert.Equal(t, "b", items[1].(string))
+	assert.Equal(t,7, list.Len())
+	assert.Equal(t, "c", list.head.Val.(string))
+
+	items = list.PopScoreLowerThan("3000", 10000)
+	assert.Equal(t, 1, len(items))
+	assert.Equal(t, "c", items[0].(string))
+	assert.Equal(t, 6, list.Len())
+	assert.Equal(t, "d", list.head.Val.(string))
+
+	items = list.PopScoreLowerThan("1000", 10000)
+	assert.Equal(t, 0, len(items))
+	assert.Equal(t, 6, list.Len())
+	assert.Equal(t, "d", list.head.Val.(string))
+
+	items = list.PopScoreLowerThan("(6000", 10000)
+	assert.Equal(t, 2, len(items))
+	assert.Equal(t, "d", items[0].(string))
+	assert.Equal(t, "e", items[1].(string))
+	assert.Equal(t, 4, list.Len())
+	assert.Equal(t, "f", list.head.Val.(string))
+}
+
+func BenchmarkSortedListSequenceAdd(b *testing.B) {
+	list := NewSortedList()
+	for i := 0; i < b.N; i++ {
+		list.Add(int64(i), "val")
+	}
+}
+
+func BenchmarkSortedListRandomAdd(b *testing.B) {
+	count := b.N
+	numbers := make([]int64, count)
+	fmt.Println(count)
+
+	rs := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(rs)
+	for i := 0; i < count; i++ {
+		numbers[i] = r.Int63()
+	}
+
+	list := NewSortedList()
+	for i := 0; i < count; i++ {
+		list.Add(numbers[i], "val")
+	}
+}
+
+func extractListToSlice(list *SortedList) []string {
+	results := make([]string, list.Len())
+	idx := 0
+	current := list.head
+	for current != nil {
+		results[idx] = current.Val.(string)
+
+		idx++
+		current = current.Next
+	}
+	return results
+}
